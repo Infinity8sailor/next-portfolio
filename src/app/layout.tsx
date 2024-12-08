b6385c37faa4f4741f5bc781379b32073f59146f
Vaibhav Dasharathe <vinfinitysailor@gmail.com>
2024-12-08 17:11:08 +0530

    Google Analytics added.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,67 +5,69 @@ import { Providers } from "./providers";
 import { Nav } from "@/components/nav";
 import { TsParticle } from "@/components/TsParticles";
 import Footer from "@/components/footer";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Infinity Sailor",
-  description: "Portfolio Website",
+    title: "Infinity Sailor",
+    description: "Portfolio Website",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en" className="scroll-smooth" suppressHydrationWarning>
-      <body
-        className={` bg-sky-100 dark:bg-slate-950 overflow-hidden flex flex-col`}
-      >
-        <Providers>
-          <TsParticle />
+    return (
+        <html lang="en" className="scroll-smooth" suppressHydrationWarning>
+            <body
+                className={` bg-sky-100 dark:bg-slate-950 overflow-hidden flex flex-col`}
+            >
+                <Providers>
+                    <TsParticle />
 
-          <>
-            <Nav
-              nav_items={[
-                {
-                  name: "Home",
-                  href: "/",
-                },
-                // {
-                //   name: "Eco-System",
-                //   href: "/EcoSystem",
-                // },
-                {
-                  name: "Elemental",
-                  href: "/elemental",
-                },
-                {
-                  name: "Admin-io",
-                  href: "/admin_io",
-                },
-                {
-                  name: "Workflows",
-                  href: "/workflows",
-                },
-                // {
-                //   name: "Gallary",
-                //   href: "Gallary",
-                // },
-                // {
-                //   name: "YT",
-                //   href: "YT",
-                // },
-              ]}
-            />
-            <div className="w-full h-full overflow-y-scroll">
-              {children}
-              <Footer />
-            </div>
-          </>
-        </Providers>
-      </body>
-    </html>
-  );
+                    <>
+                        <Nav
+                            nav_items={[
+                                {
+                                    name: "Home",
+                                    href: "/",
+                                },
+                                // {
+                                //   name: "Eco-System",
+                                //   href: "/EcoSystem",
+                                // },
+                                {
+                                    name: "Elemental",
+                                    href: "/elemental",
+                                },
+                                {
+                                    name: "Admin-io",
+                                    href: "/admin_io",
+                                },
+                                {
+                                    name: "Workflows",
+                                    href: "/workflows",
+                                },
+                                // {
+                                //   name: "Gallary",
+                                //   href: "Gallary",
+                                // },
+                                // {
+                                //   name: "YT",
+                                //   href: "YT",
+                                // },
+                            ]}
+                        />
+                        <div className="w-full h-full overflow-y-scroll">
+                            {children}
+                            <Footer />
+                        </div>
+                    </>
+                </Providers>
+            </body>
+            <GoogleAnalytics gaId="G-1QZ8K2TF4T" />
+        </html>
+    );
 }
